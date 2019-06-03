a8f34625b7295c864559819979fee780045775ca
esheleveyster <e.sheleveister@gmail.com>
2019-06-04 00:19:47 +0300

    changes

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,7 @@ const btnClose = document.querySelector('.modal__icon-close');
 const wrapperClose = document.querySelector('.modal-wrapper');
 const getToKnowBtn = document.querySelector('.get-to-know__button');
 const modalBackground = document.querySelector('.modal--background');
+const sendBtn = document.querySelector('.modal__button');
 
 
 const openModal = () => {
@@ -24,3 +25,21 @@ btnClose.addEventListener('click', closeModal);
 wrapperClose.addEventListener('click', closeModal);
 
 modalBackground.addEventListener('click', e => e.stopPropagation());
+
+
+sendBtn.addEventListener('click', e => {
+  e.preventDefault();
+
+  const xhr = new XMLHttpRequest();
+
+  const form = document.forms.feedback_form;
+  const formName = form.elements.name.value;
+  const formEmail = form.elements.email.value;
+
+  const params = `name=${encodeURIComponent(formName)}&email=${encodeURIComponent(formEmail)}`;
+  // xhr.open('GET', '/api/mail?' + params, true);
+  // xhr.send();
+
+  // console.log('formName', formName);
+  // console.log('formEmail', formEmail);
+});
